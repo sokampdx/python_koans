83aa703eab12e0b9cec58fdd4c87ad81a03e3692
sokampdx <sokam@pdx.edu>
2018-10-08 20:33:22 -0700

    python3 : done dice

--- python3/koans/about_dice_project.py
+++ python3/koans/about_dice_project.py
@@ -14,9 +14,8 @@ class DiceSet:
         return self._values
 
     def roll(self, n):
-        # Needs implementing!
         # Tip: random.randint(min, max) can be used to generate random numbers
-        pass
+        self._values = list(map(lambda x: random.randint(1, 6), range(n)))
 
 class AboutDiceProject(Koan):
     def test_can_create_a_dice_set(self):
