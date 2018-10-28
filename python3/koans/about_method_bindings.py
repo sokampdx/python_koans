dd61d109c0a61f66ff84aa929dd57daca4872b7c
sokampdx <sokam@pdx.edu>
2018-10-27 20:32:31 -0700

    python3 : done scope and modules

--- python3/koans/about_method_bindings.py
+++ python3/koans/about_method_bindings.py
@@ -25,12 +25,12 @@ class AboutMethodBindings(Koan):
 
     def test_functions_have_attributes(self):
         obj = Class()
-        self.assertEqual(34, len(dir(function)))
+        self.assertEqual(35, len(dir(function)))
         self.assertEqual(True, dir(function) == dir(obj.method.__func__))
 
     def test_methods_have_different_attributes(self):
         obj = Class()
-        self.assertEqual(26, len(dir(obj.method)))
+        self.assertEqual(27, len(dir(obj.method)))
 
     def test_setting_attributes_on_an_unbound_function(self):
         function.cherries = 3
